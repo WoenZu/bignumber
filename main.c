e25174a8c102744a5dfb55aabe86bfcf63f31f37
Andrey <0x81zu@gmail.com>
2021-03-01 16:44:09 +0300

    (feat): new func PrintArr

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int *add(int *nl, int *n2, int l1, int l2);
+void PrintArr(int *arr, int len);
+
 int main() {
-	int n1[] = {1,3,5};
-	int n2[] = {4,3,9};
+	int n1[] = {8};
+	int n2[] = {4, 3};
 	int l1 = sizeof(n1)/sizeof(n1[0]);
 	int l2 = sizeof(n2)/sizeof(n2[0]);
 	add(n1, n2, l1, l2);
@@ -13,13 +15,46 @@ int main() {
 }
 
 int *add(int *n1, int *n2, int l1,int l2) {
-	int *sum = calloc(100, sizeof(int));
+	int arrayLen = 5;
+	int *sum = calloc(arrayLen, sizeof(int));
 	int mem = 0;
-	// find biggest array
+	int i;
+	int count;
 	
-	// calculate summ
-	for() {
+	count = l1 < l2 ? l1 : l2;
+
+	// debug info
+	printf("\ndebug");
+	printf("\narray len: %d, count: %d", arrayLen, count);
+	printf("\nl1: %d, l2: %d", l1, l2);
+	
+	// debug
+
+	for(i = count; i >= 0; i--) {
+		printf("\niteration: %d", i); // debug
+		printf("\nn1: %d, n2: %d", n1[i], n2[i]);
+		
+		if (n1[i] + n2[i] > 10) {
+			printf("\n > 10 ");
+			
+			mem = 1;
+			sum[i] = n1[i] + n2[i] - 10;
+			printf("\ninner sum: %d", sum[i]);
+			
+		} else if(n1[i] + n2[i] < 10) {
+			sum[i] = n1[i] + n2[i];
+			mem = 0;
+		}
 	}
-	return sum;	
+	printf("\nsum: ");
+	PrintArr(sum, arrayLen);
+	
+	return sum;
 }
 
+void PrintArr(int *arr, int l) {
+	int i;
+	for (int i = 0; i < l; i++) {
+		printf("%d", arr[i]);
+	}
+}
